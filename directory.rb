3d007a6f3ca8c15f50373be8cc29dce4de8a4846
EC2 Default User <ec2-user@ip-172-31-46-171.eu-west-2.compute.internal>
2020-01-15 15:58:35 +0000

    Add methods

--- directory.rb
+++ directory.rb
@@ -16,15 +16,25 @@ students = [
   "Norman Bates"
 ]
 
-# and then print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-    puts student
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
 end
 
-# finally, we print the total
-puts "Overall, we have #{students.count} great students"
+# methods
+
+print_header
+print(students)
+print_footer(students)
 
 
 
