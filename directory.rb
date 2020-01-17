2f2cb965877d39a3bb03cb94f8016c347b2ba7c4
EC2 Default User <ec2-user@ip-172-31-27-60.eu-west-2.compute.internal>
2020-01-17 10:30:04 +0000

    Taking arguments from the command line

--- directory.rb
+++ directory.rb
@@ -4,21 +4,21 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -26,7 +26,8 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
-  puts "9. Exit" # 9 because we'll be adding more potion
+  puts "4. Load Students"
+  puts "9. Exit" # 9 because we'll be adding more option
 end
 
 def show_students
@@ -43,6 +44,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit # terminate the program
   else
@@ -73,19 +76,32 @@ def save_students
   file.close # close file
 end
 
-def load_students
-  file = File.open("students.csv", "r") # open and reading the file
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
-  name, cohort = line.chomp.split(',') 
+  name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil? 
+  if File.exists?(filename) 
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit 
+  end
 end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
+try_load_students
 interactive_menu
 
 
